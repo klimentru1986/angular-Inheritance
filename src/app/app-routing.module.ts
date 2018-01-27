871df93a30ba1995f48bf03c26d58825ca5c1fec
klimentru1986 <kliment1986@inbox.ru>
2018-01-27 23:07:47 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './forms/sign-up/sign-up.component';
+import { SignInComponent } from './forms/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'sign-in', component: SignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './controls/input/input.component';
+import { SelectComponent } from './controls/select/select.component';
+import { SignUpComponent } from './forms/sign-up/sign-up.component';
+import { SignInComponent } from './forms/sign-in/sign-in.component';
+import { CheckboxComponent } from './controls/checkbox/checkbox.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    SelectComponent,
+    SignUpComponent,
+    SignInComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule,
